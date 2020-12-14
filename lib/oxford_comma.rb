e8d10bce9bd33d2509e423ee3ef711046f8d3570
remote-torrent-6747 <abbyc@sky.com>
2020-12-14 15:57:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     return array.join(", ")
   else
-    last_position = "and #{array[-1]}"
-    return new_array.join(", ")
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
   end
 end
